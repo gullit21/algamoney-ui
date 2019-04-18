3e5a9c85f7ed0d3c57ddc1e00e49ab489496de7f
Gullit Menezes <gullit.menezes@gmail.com>
2019-04-18 16:41:00 -0300

    10.5. Adicionando uma tabela de dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {InputTextModule} from 'primeng/components/inputtext/inputtext';
 import {ButtonModule} from 'primeng/components/button/button';
-import { DataTableModule } from 'primeng/components/datatable/datatable';
+import {TableModule} from 'primeng/table';
 
 import { AppComponent } from './app.component';
 
@@ -14,7 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     InputTextModule,
     ButtonModule,
-    DataTableModule
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
