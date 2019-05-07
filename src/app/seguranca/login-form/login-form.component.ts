4d30fcded22403eb45860441dacf8e6a2f5e39d5
Gullit Menezes <gullit.menezes@gmail.com>
2019-05-07 11:25:37 -0300

    19.6. Tratando casos de erros e sucesso de autenticação

--- src/app/seguranca/login-form/login-form.component.ts
+++ src/app/seguranca/login-form/login-form.component.ts
@@ -1,5 +1,7 @@
+import { ErrorHandlerService } from './../../core/error-handler.service';
 import { AuthService } from './../auth.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -9,17 +11,31 @@ import { Component } from '@angular/core';
 export class LoginFormComponent {
 
   constructor(
-      private authService: AuthService
+      private authService: AuthService,
+      private errorHandlerService: ErrorHandlerService,
+      private router: Router
   ) { }
 
   login(usuario: string, senha: string) {
     this.authService.login(usuario, senha).subscribe(
         response => {
             this.authService.armazenarToken(response.access_token);
-            console.log(response);
+            this.router.navigate(['/lancamentos']);
         },
-        error => {
-            console.log(error);
+        responseError => {
+            let msg = '';
+
+            if (responseError.status === 400) {
+
+                if (responseError.error.error === 'invalid_grant') {
+                    msg = 'Usuário ou senha inválida!';
+                    this.errorHandlerService.handle(msg);
+
+                    return;
+                }
+            }
+
+            this.errorHandlerService.handle(responseError);
         }
     );
   }
